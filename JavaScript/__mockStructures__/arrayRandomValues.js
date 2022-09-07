b4a70b1bca47580439b51f5d80be2a40de5ce0d7
DarkCoderDev <69434077+DarkCoderDev@users.noreply.github.com>
2022-09-07 23:41:39 +0300

    Update arrayRandomValues.js

--- JavaScript/__mockStructures__/arrayRandomValues.js
+++ JavaScript/__mockStructures__/arrayRandomValues.js
@@ -1,3 +1,3 @@
-const mockArray = [8, 24, 90, 43, 11, 52, 59, 72, 53, 41, 19, 22, 6, 96, 33];
+const array = [8, 24, 90, 43, 11, 52, 59, 72, 53, 41, 19, 22, 6, 96, 33];
 
-module.exports = mockArray;
+module.exports = array;
